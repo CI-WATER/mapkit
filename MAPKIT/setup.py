79d30621404229d989cc2625e88a2c2e4db2124e
Nathan Swain <nathan.swain86@gmail.com>
2013-12-17 13:44:37 -0700

    First commit.

---
+++ MAPKIT/setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import setup, find_packages
+
+requires = [
+    'sqlalchemy>=0.8',
+    'geoalchemy2'
+    ]
+
+setup(name='gsshapy',
+      version='1.1.0',
+      description='An SQLAlchemy ORM for GSSHA model files.',
+      long_description='',
+      author='Nathan Swain',
+      author_email='nathan.swain@byu.net',
+      url='https://bitbucket.org/swainn/gsshapy',
+      license='BSD 2-Clause License',
+      keywords='gssha database model',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite='gsshapy.tests.all_tests'
+      )
