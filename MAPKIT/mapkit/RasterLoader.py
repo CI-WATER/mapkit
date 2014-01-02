5e89880132e6a7a1339c870bc3711d94ac212c94
Nathan Swain <nathan.swain86@gmail.com>
2014-01-02 14:09:21 -0700

    Added very basic raster loader for mapkit.

--- MAPKIT/mapkit/RasterLoader.py
+++ MAPKIT/mapkit/RasterLoader.py
@@ -8,6 +8,8 @@
 ********************************************************************************
 '''
 
+import subprocess, os, datetime
+
 class RasterLoader(object):
     '''
     An instance of RasterLoader can be used to load rasters into a 
@@ -16,8 +18,94 @@ class RasterLoader(object):
     '''
 
 
-    def __init__(self):
+    def __init__(self, sqlAlchemySession, raster2pgsql=''):
         '''
         Constructor
         '''
+        self._session = sqlAlchemySession
+        self._raster2pgsql = raster2pgsql
+        
+    def load(self, tableName='rasters', rasters=[]):
+        '''
+        Accepts a list of paths to raster files to load into the database.
+        Returns the ids of the rasters loaded successfully in the same order
+        as the list passed in.
+        '''
+        
+        for raster in rasters:
+            # Must read in using the raster2pgsql commandline tool.
+            rasterPath = raster['path']
+
+            if 'srid' in raster:
+                srid = str(raster['srid'])
+            else:
+                srid = '4326'
+                
+            if 'no-data' in raster:
+                noData = str(raster['no-data'])
+            else:
+                noData = '-1'
+                                
+            raster2pgsqlProcess = subprocess.Popen(
+                                                   [
+                                                    self._raster2pgsql,
+                                                    '-s',
+                                                    srid,
+                                                    '-N',
+                                                    noData,
+                                                    rasterPath, 
+                                                    'n_a'
+                                                   ],
+                                                   stdout=subprocess.PIPE
+                                                  )
+            
+            # This commandline tool generates the SQL to load the raster into the database
+            # However, we want to use SQLAlchemy to load the values into the database. 
+            # We do this by extracting the value from the sql that is generated.
+            sql, error = raster2pgsqlProcess.communicate()        
+            
+            if sql:
+                # This esoteric line is used to extract only the value of the raster (which is stored as a Well Know Binary string)
+                
+                # Example of Output:
+                # BEGIN;
+                # INSERT INTO "idx_index_maps" ("rast") VALUES ('0100...56C096CE87'::raster);
+                # END;
+                
+                # The WKB is wrapped in single quotes. Splitting on single quotes isolates it as the 
+                # second item in the resulting list.
+                wellKnownBinary =  sql.split("'")[1]
+                
+            else:
+                print error
+                raise
+                
+            rasterBinary = wellKnownBinary
+            
+            # Get the filename
+            filename = os.path.split(rasterPath)[1]
+            
+            # Construct statement
+            if 'timestamp' in raster:
+                timestamp = raster['timestamp']
+                
+                statement = '''
+                            BEGIN;
+                            CREATE TABLE IF NOT EXISTS "{0}" ("id" serial PRIMARY KEY, "raster" raster,"filename" text, "timestamp" timestamp);
+                            INSERT INTO "{0}" ("raster","filename","timestamp") VALUES ('{1}'::raster,'{2}','{3}') RETURNING "id";
+                            END;
+                            '''.format(tableName, rasterBinary, filename, timestamp)
+            else:
+                statement = '''
+                            BEGIN;
+                            CREATE TABLE IF NOT EXISTS "{0}" ("id" serial PRIMARY KEY, "raster" raster,"filename" text, "timestamp" timestamp);
+                            INSERT INTO "{0}" ("raster","filename") VALUES ('{1}'::raster,'{2}') RETURNING "id";
+                            END;
+                            '''.format(tableName, rasterBinary, filename)
+                        
+            result = self._session.execute(statement)
+            
+            
+        
+        
         
---
+++ MAPKIT/mapkit/examples/raster_loader_load.py
@@ -0,0 +1,40 @@
+from mapkit.RasterLoader import RasterLoader
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from datetime import datetime
+
+# Setup SQLAlchemy connection
+gsshapyEngine = create_engine('postgresql://swainn:(|w@ter@localhost:5432/gsshapy_postgis')
+gsshapySessionMaker = sessionmaker(bind=gsshapyEngine)
+session = gsshapySessionMaker()
+
+# Initialize raster converter
+loader = RasterLoader(sqlAlchemySession=session,
+                      raster2pgsql='/Applications/Postgres93.app/Contents/MacOS/bin/raster2pgsql')
+
+# Create list of dictionaries with the raster information rasters to load
+rasterPaths = [{'path': '/Users/swainn/projects/post_gis/rasters/combo.idx', 
+                'srid': 26912,
+                'no-data': 0,
+                'timestamp': datetime.today()},
+               {'path': '/Users/swainn/projects/post_gis/rasters/luse.idx',
+                'srid': 26912,
+                'no-data': -1,
+                'timestamp': datetime.today()},
+               {'path': '/Users/swainn/projects/post_gis/rasters/parkcity.ele',
+                'srid': 26912,
+                'no-data': 0},
+               {'path': '/Users/swainn/projects/post_gis/rasters/parkcity.msk',
+                'srid': 26912,
+                'no-data': 0},
+               {'path': '/Users/swainn/projects/post_gis/rasters/soil.idx',
+                'srid': 26912,
+                'no-data': -1,
+                'timestamp': datetime.today()}]
+
+
+# Execute load method
+result = loader.load('test_rasters', rasterPaths)
+
+
